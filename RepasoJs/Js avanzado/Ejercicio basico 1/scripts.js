877affa28eaa51eb3b9af1b7625241d9105cc076
WaterLord404 <davidgomezrodriguezinfor@gmail.com>
2020-09-23 09:25:01 +0200

    Ejercicio enventos js avanzado

---
+++ RepasoJs/Js avanzado/Ejercicio basico 1/scripts.js
@@ -0,0 +1,46 @@
+let box = document.getElementById("box");
+let text = document.getElementById("text");
+let boton = document.getElementById("boton");
+let input = document.getElementById("input");
+let gallery = document.getElementById("gallery")
+
+box.addEventListener("mouseenter", () => {
+    box.setAttribute("class", "verde");
+});
+
+box.addEventListener("mouseleave", () => {
+    box.setAttribute("class", "rojo");
+});
+
+box.addEventListener("mousedown", () => {
+    console.log("Has pulsado la caja");
+});
+
+box.addEventListener("mouseup", () => {
+    console.log("Has soltado el boton izquierdo dentro de la caja")
+});
+
+text.addEventListener("keydown", (e) => {
+    console.log("Has pulsado una tecla -> " + e.key);
+});
+
+text.addEventListener("keyup", (e) => {
+    console.log("Has soltado una tecla -> " + e.key);
+});
+
+
+boton.addEventListener("click", () => {
+
+    input.addEventListener("keyup", (e) => {
+        console.log(e.key);
+    });
+});
+
+
+gallery.addEventListener("click", (e) => {
+    if (e.target.className == "selected") {
+        e.target.setAttribute("class", "gallery__item")
+    } else {
+        e.target.setAttribute("class", "selected");
+    }
+});
